c56041d6d77825fe0a96c8e9c095aea35d61e6dd
sheier-tomer <tomer.sheier@gmail.com>
2023-12-18 22:26:00 -0500

    fixed formatting

--- js/scrabbleGame.js
+++ js/scrabbleGame.js
@@ -4,7 +4,6 @@
 //I also utilized the jquery UI library to make the tiles draggable and droppable
 //I also used the jquery UI library to make the tiles snap to the board
 
-
 var ScrabbleTiles = [];
 ScrabbleTiles["A"] = { "value": 1, "original": 9, "remaining": 9 };
 ScrabbleTiles["B"] = { "value": 3, "original": 2, "remaining": 2 };
@@ -34,9 +33,8 @@ ScrabbleTiles["Y"] = { "value": 4, "original": 2, "remaining": 2 };
 ScrabbleTiles["Z"] = { "value": 10, "original": 1, "remaining": 1 };
 ScrabbleTiles["["] = { "value": 0, "original": 2, "remaining": 2 };
 
-
-var tileSet = [];//for if there is a tile on the square
-var playerRack = [];//players tiles
+var tileSet = []; //for if there is a tile on the square
+var playerRack = []; //players tiles
 var score = 0;
 var ScrabbleValues = Object.keys(ScrabbleTiles).length;
 
@@ -56,30 +54,33 @@ function Rack() {
     //loop through the rack and add tiles to it
     for (var i = 0; tileCount <= 7; i++) {
 
-        var randTile = Math.floor((Math.random() * 27));//get a random tile
+        var randTile = Math.floor((Math.random() * 27)); //get a random tile
 
         //This condition verifies whether there are still tiles remaining and the tile bag is not empty. 
         //If there are tiles left and the bag is not empty, it adds a randomly selected tile, 
         //reduces the remaining ScrabbleValue, and includes the image associated with the chosen tile.
         if (ScrabbleTiles[String.fromCharCode(65 + randTile)]["remaining"] !== 0 && remainingTiles()) {
-            
-            playerRack[tileCount] = { "letter": String.fromCharCode(65 + randTile), "value": ScrabbleTiles[String.fromCharCode(65 + randTile)]["value"] };
 
-            ScrabbleTiles[String.fromCharCode(65 + randTile)]["remaining"]--;//reduce the remaining tiles
+            playerRack[tileCount] = {
+                "letter": String.fromCharCode(65 + randTile),
+                "value": ScrabbleTiles[String.fromCharCode(65 + randTile)]["value"]
+            };
+
+            ScrabbleTiles[String.fromCharCode(65 + randTile)]["remaining"]--; //reduce the remaining tiles
 
             id = "tile" + tileCount;
             title = playerRack[tileCount]["letter"];
-            src = "./graphics_data/Scrabble_tiles/Scrabble_Tile_" + playerRack[tileCount]["letter"] + ".jpg";//src link
+            src = "./graphics_data/Scrabble_tiles/Scrabble_Tile_" + playerRack[tileCount]["letter"] + ".jpg"; //src link
 
             $('#playerRack').prepend($('<img>', {
                 id: id,
                 src: src,
                 class: tileClass,
                 title: title
-            }));//add the tile to the rack
+            })); //add the tile to the rack
 
 
-            tileCount++;//increment the tile count
+            tileCount++; //increment the tile count
         }
 
         //This condition verifies whether there are still tiles remaining and the tile bag is not empty.
@@ -100,9 +101,6 @@ function Rack() {
 
     updateScore();
 }
-
-
-
 
 //function for drag and drop functionality.
 function tileOnBoard(event, ui) {
@@ -128,7 +126,6 @@ function tileOnBoard(event, ui) {
     }
 }
 
-
 //subtract the value when the tile is removed,
 function tileOffBoard(event, ui) {
     const tileId = $(this).attr("id");
@@ -153,8 +150,6 @@ function tileOffBoard(event, ui) {
     }
 }
 
-
-
 function reset() {
 
     $("#newTilesButton").prop("disabled", false);
@@ -168,10 +163,6 @@ function reset() {
     updateScore();
     Rack();
 }
-
-
-
-
 
 //update the score
 function updateScore() {
@@ -205,19 +196,21 @@ function remainingTiles() {
 
 
         if (ScrabbleTiles[String.fromCharCode(65 + count)]["remaining"] !== 0) {
-            tileSet = true;//if the tiles remaining is not 0, there are tiles left
+            tileSet = true; //if the tiles remaining is not 0, there are tiles left
         }
-        count++;//increment the count
+        count++; //increment the count
     }
 
     return tileSet;
 
 }
 
-
-$(document).ready(function () {
+$(document).ready(function() {
 
     Rack();
-    $(".boardTile").droppable({ drop: tileOnBoard, out: tileOffBoard });
+    $(".boardTile").droppable({
+        drop: tileOnBoard,
+        out: tileOffBoard
+    });
 
 });
